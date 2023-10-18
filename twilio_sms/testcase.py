55fa4e8ac2e7682cd80ad80e798c06bf40d3ecd3
Suzzene <116281847+Suzzene@users.noreply.github.com>
2023-10-18 23:56:23 +0530

    Added sms

---
+++ twilio_sms/testcase.py
@@ -0,0 +1,43 @@
+import pytest
+import function as otp
+import unittest
+
+
+class TestOTPFunctions(unittest.TestCase):
+
+    def test_generateOtp(self):
+        """Test the generateOtp function."""
+        result = otp.generateOtp(8)
+        expected_len = 8
+
+        result_isint = result.isdigit()
+        expected_isint = True #if number contains all digits
+
+        self.assertEqual(len(result), expected_len)
+        self.assertEqual(result_isint, expected_isint)
+        
+
+    def test_validateEmailID(self):
+        """Test validate Email."""
+        result = otp.validateEmailID("test@gmail.com")
+        expected = True
+
+        self.assertEqual(result, expected)
+
+        result = otp.validateEmailID("test@dbatu.ac.in")
+
+        self.assertEqual(result, expected)
+        
+
+    def test_validateMobile(self):
+        """Test validate Mobile number."""
+        mobile = "8600263475"
+        result = otp.validateMobile(mobile)
+        expected = True
+
+        self.assertEqual(result, expected)
+    
+
+    
+if __name__ == "__main__":
+    pytest.main()
